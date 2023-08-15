1e234d26847eda2311b9138b1221ba2f274a8e4d
08201356 <nguyenhoainma0613@gmail.com>
2023-08-15 09:39:03 +0700

    add work

---
+++ Refactoring/src/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+    public static int calculate(int a, int b, char operator) {
+        switch (operator){
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b != 0)
+                    return a / b;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
